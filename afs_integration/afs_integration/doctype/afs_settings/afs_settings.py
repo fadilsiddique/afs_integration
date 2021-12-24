3652cdc803ef8bb7295229a4b363efb1c5476956
fadilsid <fadil@tridz.com>
2021-12-24 05:43:08 +0000

    env file updated

--- afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
+++ afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021, Tridz Technologies and contributors
 # For license information, please see license.txt
 from __future__ import unicode_literals
+from requests.api import get
 
 from requests.sessions import session
 import frappe
@@ -10,7 +11,8 @@ from frappe import _,request
 from frappe.model.document import Document
 from frappe.utils import get_url, call_hook_method, cint, get_timestamp
 from frappe.integrations.utils import (make_get_request, make_post_request, create_request_log,
-	create_payment_gateway)	
+	create_payment_gateway)
+import os
 
 
 
@@ -31,7 +33,7 @@ class AfsSettings(Document):
 
 	def validate_transaction_currency(self, currency):
 		if currency not in self.supported_currencies:
-			frappe.throw(_("Please select another payment method. Paystack does not support transactions in currency '{0}'").format(currency))
+			frappe.throw(_("Please select another payment method. Afs does not support transactions in currency '{0}'").format(currency))
 
 	def get_payment_url(self, **kwargs):
 		'''Return payment url with several params'''
@@ -44,16 +46,19 @@ class AfsSettings(Document):
 		return integration_request
 
 @frappe.whitelist()
+
 def get_payment_info(order_id,dt,dn):
+	from dotenv import load_dotenv
+	load_dotenv()
 
 	if request.method=='POST':
 		payment_request = frappe.get_doc('Payment Request',order_id)
-		url="https://afs.gateway.mastercard.com/api/rest/version/61/merchant/TEST100078691/session/"
+		url=os.environ.get("url")
 		payload={
 			"apiOperation":"CREATE_CHECKOUT_SESSION",
 			"interaction":{
 				"operation":"PURCHASE",
-				"returnUrl":"http://dev-zoom.tridz.in/"
+				"returnUrl":os.environ.get("return_url")
 			},
 			"order":{
 				"amount":payment_request.grand_total,
@@ -71,7 +76,7 @@ def get_payment_info(order_id,dt,dn):
 		print(payload)
 
 		headers = {
-					'Authorization': 'Basic bWVyY2hhbnQuVEVTVDEwMDA3ODY5MTowMGJhN2RlMDVkOTI1ODQ5YjRlNzk2MTE4NTZmMDVkMg==',
+					'Authorization': os.environ.get("auth"),
 					'Content-Type': 'text/plain'
 					}
 		response = requests.request("POST", url=url, headers=headers, data=payload)
