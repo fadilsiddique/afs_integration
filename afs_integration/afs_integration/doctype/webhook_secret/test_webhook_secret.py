b231e378b878c65a1b73b7f6feffdb5e0102f9f2
fadilsid <fadil@tridz.com>
2022-01-19 14:35:18 +0000

    webhook changes

---
+++ afs_integration/afs_integration/doctype/webhook_secret/test_webhook_secret.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Tridz Technologies and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestWebhookSecret(unittest.TestCase):
+	pass
--- afs_integration/webhook.py
+++ afs_integration/webhook.py
@@ -1,4 +1,7 @@
+from http.client import OK
+from urllib import request
 import frappe
+import requests
 import json
 from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
 
@@ -6,26 +9,32 @@ from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
 @frappe.whitelist(allow_guest=True)
 
 def webhook(**kwargs):
-    data=json.loads(frappe.request.data)
-    print(data)
-    status=data.get('result')
-    order_id=data.get('id')
-    amount=data.get('amount')
+    head=frappe.request.headers['X-Notification-Secret']
+    secret=frappe.get_single('Webhook Secret').secret
+    print(head)
 
-    pay_req=frappe.get_doc('Payment Request',order_id)
-    reference_doc_id=pay_req.get('reference_name')
-    reference_doc=pay_req.get('reference_doctype')
-    sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
-    total=sales_order_doc.get('total')
+    if head==secret:
 
-    
+        data=json.loads(frappe.request.data)
+        print(data)
+        status=data.get('result')
+        order_id=data.get('id')
+        amount=data.get('amount')
 
-    if status=='SUCCESS':
-       invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
-       invoice.submit()
+        pay_req=frappe.get_doc('Payment Request',order_id)
+        reference_doc_id=pay_req.get('reference_name')
+        reference_doc=pay_req.get('reference_doctype')
+        sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
+        total=sales_order_doc.get('total')
 
-       invoice_doc=frappe.get_doc('Sales Invoice',invoice.name)
-       return invoice
+        
+
+        if status=='SUCCESS':
+            invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
+            invoice.submit()
+
+        invoice_doc=frappe.get_doc('Sales Invoice',invoice.name)
+        return invoice
 
 # def invoice_testing(source_name,test_id):
 #     if test_id==1:
