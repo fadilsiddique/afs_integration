febdec1372afe648e390188839d1a32525b3e485
fadilsid <fadil@tridz.com>
2022-04-29 06:37:08 +0000

    test: webhook feature

--- afs_integration/webhook.py
+++ afs_integration/webhook.py
@@ -1,3 +1,4 @@
+from email import header
 from http.client import OK
 from urllib import request, response
 import frappe
@@ -10,9 +11,10 @@ import os
 @frappe.whitelist(allow_guest=True)
 def webhookData():
     x=frappe.request.data
+    header=frappe.request.headers['X-Notification-Secret']
 
     try:
-        y=json.loads(frappe.request.data)
+        y=json.loads(x)
     except:
         pass
     # if frappe.request.headers['X-Notification-Secret']=='CA30951A5324FCCC66EFE9C4890E93A5':
@@ -20,23 +22,26 @@ def webhookData():
         # load_dotenv()
     data=json.loads(frappe.request.data)
     # secret=os.environ.get('secret')
-    doc=frappe.new_doc('Webhook Capture')
-    doc.webhook_response=str(data)
-    doc.insert(ignore_permissions=True)
-    doc.save(ignore_permissions=True)
-    data=json.loads(frappe.request.data)
-    status=data.get('result')
-    order_id=data.get('id')
-    amount=data.get('amount')
-    pay_req=frappe.get_doc('Payment Request',order_id)
-    reference_doc_id=pay_req.get('reference_name')
-    reference_doc=pay_req.get('reference_doctype')
-    sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
-    total=sales_order_doc.get('total')
-    if status=='SUCCESS':
-        invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
-        invoice.submit()
-        return invoice
+    if header=='CA30951A5324FCCC66EFE9C4890E93A5':
+        doc=frappe.new_doc('Webhook Capture')
+        doc.webhook_response=str(data)
+        doc.insert(ignore_permissions=True)
+        doc.save(ignore_permissions=True)
+        data=json.loads(frappe.request.data)
+        status=data.get('result')
+        order_id=data.get('id')
+        amount=data.get('amount')
+        pay_req=frappe.get_doc('Payment Request',order_id)
+        reference_doc_id=pay_req.get('reference_name')
+        reference_doc=pay_req.get('reference_doctype')
+        sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
+        total=sales_order_doc.get('total')
+        if status=='SUCCESS':
+            invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
+            invoice.submit()
+            
+    return invoice
+    
 
 
 
