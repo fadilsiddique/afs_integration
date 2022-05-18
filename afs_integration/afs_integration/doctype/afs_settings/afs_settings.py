d9983698ca64088300b6dfca4bb97d9915ecd44f
fadilsid <fadil@tridz.com>
2022-05-18 11:29:08 +0000

    fix: webhook path changed

--- afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
+++ afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
@@ -8,6 +8,7 @@ import frappe
 import requests
 import json
 from frappe import _,request
+from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
 from frappe.model.document import Document
 from frappe.utils import get_url, call_hook_method, cint, get_timestamp
 from frappe.integrations.utils import (make_get_request, make_post_request, create_request_log,
@@ -85,10 +86,42 @@ def get_payment_info(order_id,dt,dn):
 		return response.json()
 
 
-# webhook
-# @frappe.whitelist(allow_guest=True)
-# def webhook(request):
-# 	print(request)
+@frappe.whitelist(allow_guest=True)
+def webhook():
+    header=frappe.request.headers['x-notification-secret']
+    # if frappe.request.headers['X-Notification-Secret']=='CA30951A5324FCCC66EFE9C4890E93A5':
+        # from dotenv import load_dotenv
+        # load_dotenv()
+    
+    # secret=os.environ.get('secret')
+    if header=='CA30951A5324FCCC66EFE9C4890E93A5':
+        data=json.loads(frappe.request.data)
+        doc=frappe.new_doc('Webhook Capture')
+        doc.webhook_response=str(data)
+        doc.insert(ignore_permissions=True)
+        doc.save(ignore_permissions=True)
+        data=json.loads(frappe.request.data)
+        status=data.get('result')
+        order_id=data.get('id')
+        amount=data.get('amount')
+        pay_req=frappe.get_doc('Payment Request',order_id)
+        reference_doc_id=pay_req.get('reference_name')
+        reference_doc=pay_req.get('reference_doctype')
+        sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
+        total=sales_order_doc.get('total')
+        if status=='SUCCESS':
+            invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
+            invoice.submit()
+
+            return invoice
+        
+    else:
+        doc=frappe.new_doc('Webhook Capture')
+        doc.webhook_response=str(header)
+        doc.insert(ignore_permissions=True)
+        doc.save(ignore_permissions=True)
+        
+        return doc
 
 
 	
--- afs_integration/webhook.py
+++ afs_integration/webhook.py
@@ -1,52 +1,52 @@
-from distutils.log import error
-from email import header
-from http import HTTPStatus
-from http.client import OK
-from urllib import request, response
-from urllib.error import HTTPError
-import frappe
-import requests
-import json
-from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
-import os
+# from distutils.log import error
+# from email import header
+# from http import HTTPStatus
+# from http.client import OK
+# from urllib import request, response
+# from urllib.error import HTTPError
+# import frappe
+# import requests
+# import json
+# from erpnext.selling.doctype.sales_order.sales_order import make_sales_invoice
+# import os
 
 
-@frappe.whitelist(allow_guest=True)
-def webhookData():
-    header=frappe.request.headers['x-notification-secret']
-    # if frappe.request.headers['X-Notification-Secret']=='CA30951A5324FCCC66EFE9C4890E93A5':
-        # from dotenv import load_dotenv
-        # load_dotenv()
+# @frappe.whitelist(allow_guest=True)
+# def webhookData():
+#     header=frappe.request.headers['x-notification-secret']
+#     # if frappe.request.headers['X-Notification-Secret']=='CA30951A5324FCCC66EFE9C4890E93A5':
+#         # from dotenv import load_dotenv
+#         # load_dotenv()
     
-    # secret=os.environ.get('secret')
-    if header=='CA30951A5324FCCC66EFE9C4890E93A5':
-        data=json.loads(frappe.request.data)
-        doc=frappe.new_doc('Webhook Capture')
-        doc.webhook_response=str(data)
-        doc.insert(ignore_permissions=True)
-        doc.save(ignore_permissions=True)
-        data=json.loads(frappe.request.data)
-        status=data.get('result')
-        order_id=data.get('id')
-        amount=data.get('amount')
-        pay_req=frappe.get_doc('Payment Request',order_id)
-        reference_doc_id=pay_req.get('reference_name')
-        reference_doc=pay_req.get('reference_doctype')
-        sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
-        total=sales_order_doc.get('total')
-        if status=='SUCCESS':
-            invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
-            invoice.submit()
+#     # secret=os.environ.get('secret')
+#     if header=='CA30951A5324FCCC66EFE9C4890E93A5':
+#         data=json.loads(frappe.request.data)
+#         doc=frappe.new_doc('Webhook Capture')
+#         doc.webhook_response=str(data)
+#         doc.insert(ignore_permissions=True)
+#         doc.save(ignore_permissions=True)
+#         data=json.loads(frappe.request.data)
+#         status=data.get('result')
+#         order_id=data.get('id')
+#         amount=data.get('amount')
+#         pay_req=frappe.get_doc('Payment Request',order_id)
+#         reference_doc_id=pay_req.get('reference_name')
+#         reference_doc=pay_req.get('reference_doctype')
+#         sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
+#         total=sales_order_doc.get('total')
+#         if status=='SUCCESS':
+#             invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
+#             invoice.submit()
 
-            return invoice
+#             return invoice
         
-    else:
-        doc=frappe.new_doc('Webhook Capture')
-        doc.webhook_response=str(header)
-        doc.insert(ignore_permissions=True)
-        doc.save(ignore_permissions=True)
+#     else:
+#         doc=frappe.new_doc('Webhook Capture')
+#         doc.webhook_response=str(header)
+#         doc.insert(ignore_permissions=True)
+#         doc.save(ignore_permissions=True)
         
-        return doc
+#         return doc
     
 
 
