0dc0ffdb6451460bb48a66fb83705c1ee68b2693
fadilsid <fadil@tridz.com>
2022-05-18 16:57:58 +0000

    test: webhook test id

--- afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
+++ afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
@@ -97,24 +97,26 @@ def webhook():
     # secret=os.environ.get('secret')
 	if header=='CA30951A5324FCCC66EFE9C4890E93A5':
 		data=json.loads(frappe.request.data)
+		status=data.get('result')
+		print(status)
 		doc=frappe.new_doc('Webhook Capture')
 		doc.webhook_response=str(data)
 		doc.insert(ignore_permissions=True)
 		doc.save(ignore_permissions=True)
 		
 		# status=data.get('result')
-		# order_id=data.get('id')
-		# amount=data.get('amount')
-		# pay_req=frappe.get_doc('Payment Request',order_id)
-		# reference_doc_id=pay_req.get('reference_name')
-		# reference_doc=pay_req.get('reference_doctype')
-		# sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
-		# total=sales_order_doc.get('total')
-		# if status=='SUCCESS':
-		# 	invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
-		# 	invoice.submit()
+		order_id=data.get('id')
+		amount=data.get('amount')
+		pay_req=frappe.get_doc('Payment Request',order_id)
+		reference_doc_id=pay_req.get('reference_name')
+		reference_doc=pay_req.get('reference_doctype')
+		sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
+		total=sales_order_doc.get('total')
+		if status=='SUCCESS':
+			invoice= make_sales_invoice(source_name=reference_doc_id,ignore_permissions=True)
+			invoice.submit()
 
-		# 	return invoice
+			return invoice
         
 	# else:
 	#     doc=frappe.new_doc('Webhook Capture')
