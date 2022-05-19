ad64c3e009b79d3da715ac88e8e572fcb2f2ee91
fadilsid <fadil@tridz.com>
2022-05-19 06:46:03 +0000

    fix: webhook reference doctype issue fixed

--- afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
+++ afs_integration/afs_integration/doctype/afs_settings/afs_settings.py
@@ -104,19 +104,17 @@ def webhook():
 		doc.insert(ignore_permissions=True)
 		doc.save(ignore_permissions=True)
 		
-		# # status=data.get('result')
-		# order_id=data.get('id')
-		# amount=data.get('amount')
-		# pay_req=frappe.get_doc('Payment Request',order_id)
-		# reference_doc_id=pay_req.get('reference_name')
-		# reference_doc=pay_req.get('reference_doctype')
-		# sales_order_doc=frappe.get_doc(reference_doc,reference_doc_id)
-		# total=sales_order_doc.get('total')
-		# if status=='SUCCESS':
-		# 	invoice= make_sales_invoice(reference_doc_id)
-		# 	invoice.submit()
-
-		# 	return invoice
+		# status=data.get('result')
+		order_id=data.get('id')
+		amount=data.get('amount')
+		pay_req=frappe.get_doc('Payment Request',order_id)
+		reference_doc_id=pay_req.get('reference_name')
+		reference_doc=pay_req.get('reference_doctype')
+		sales_order_doc=frappe.get_doc('Sales Order',reference_doc_id)
+		total=sales_order_doc.get('total')
+		if status=='SUCCESS':
+			invoice= make_sales_invoice(reference_doc_id,target=None,ignore_permissions=True)
+			invoice.submit()
         
 	# else:
 	#     doc=frappe.new_doc('Webhook Capture')
